b290ae9175d8f511dd9a6d511c9fd9c90b580b89
Kyle Bloom <kyle@symless.com>
2021-03-04 09:10:24 +0000

    Add ssh call to ensure the remote path

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,8 @@ echo -e "${INPUT_KEY}" >__TEMP_INPUT_KEY_FILE
 
 chmod 600 __TEMP_INPUT_KEY_FILE
 
+if [[ -z "${INPUT_ENSUREREMOTE}" ]]; then
+  ssh -o StrictHostKeyChecking=no -v -i __TEMP_INPUT_KEY_FILE -p "${INPUT_PORT}" "${INPUT_USER}"@"${INPUT_HOST}" "mkdir -p ${INPUT_REMOTE}"
+fi
+
 scp -o StrictHostKeyChecking=no -v -i __TEMP_INPUT_KEY_FILE -P "${INPUT_PORT}" -r ${INPUT_LOCAL} "${INPUT_USER}"@"${INPUT_HOST}":"${INPUT_REMOTE}"
